20deaa4c00f64190f910eb6a3822746d77bc0d56
Mips2648 <mips2648@gmail.com>
2024-04-14 17:32:10 +0200

    add config

--- jeedomdaemon/base_config.py
+++ jeedomdaemon/base_config.py
@@ -1,28 +1,41 @@
+import argparse
+
 class BaseConfig(object):
-    def __init__(self, **kwargs):
-        self._kwargs = kwargs
+    def __init__(self):
+        self.__parser = argparse.ArgumentParser(description='Daemon for Jeedom plugin')
+        self.add_argument("--loglevel", help="Log Level for the daemon", type=str, default='error')
+        self.add_argument("--sockethost", help="Socket host", type=str, default='127.0.0.1')
+        self.add_argument("--socketport", help="Socket Port", type=int, default=0)
+        self.add_argument("--callback", help="Jeedom callback url", type=str)
+        self.add_argument("--apikey", help="Plugin API Key", type=str)
+        self.add_argument("--pid", help="daemon pid", type=str)
+
+    def add_argument(self, *args, **kwargs):
+        return self.__parser.add_argument(*args, **kwargs)
+
+    def parse(self):
+        self._args = self.__parser.parse_args()
 
     @property
     def callback_url(self):
-        return str(self._kwargs.get('callback', ''))
+        return str(self._args.callback)
 
     @property
     def socket_host(self):
-        return str(self._kwargs.get('sockethost', '127.0.0.1'))
+        return str(self._args.sockethost)
 
     @property
     def socket_port(self):
-        return int(self._kwargs.get('socketport', 0))
+        return int(self._args.socketport)
 
     @property
     def log_level(self):
-        return str(self._kwargs.get('loglevel', 'error'))
+        return str(self._args.loglevel)
 
     @property
     def api_key(self):
-        return str(self._kwargs.get('apikey', ''))
+        return str(self._args.apikey)
 
     @property
     def pid_filename(self):
-        # FIXME: define default pid file
-        return str(self._kwargs.get('pid', '/tmp/kroombad.pid'))
+        return str(self._args.pid)
