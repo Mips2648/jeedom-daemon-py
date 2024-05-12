4ca826c95dc966ffbf3f502b96d7eb5ea008d88d
Mips2648 <mips2648@gmail.com>
2024-05-12 10:12:14 +0200

    test publish

--- setup.py
+++ setup.py
@@ -13,11 +13,24 @@ setup(
     # Needed for dependencies
     install_requires=['aiohttp'],
     # *strongly* suggested for sharing
-    version='0.8.5',
+    version='0.8.6',
     # The license can be anything you like
     license='MIT',
     description='A base to implement Jeedom daemon in python',
     # We will also need a readme eventually (there will be a warning)
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
+    keywords = ['JEEDOM', 'DAEMON', 'ASYNCIO'],
+    classifiers=[
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+    ],
 )
