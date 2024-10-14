969ee5d04495c7c70f34d005356e6f499b95f6f5
Mips2648 <mips2648@gmail.com>
2024-10-14 09:47:21 +0200

    switch Development Status to stable for next release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords = ['JEEDOM', 'DAEMON', 'ASYNCIO'],
     classifiers=[
-        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
