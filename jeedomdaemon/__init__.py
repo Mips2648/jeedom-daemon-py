06c8ba263fc09b90b2643450a74744a49d6b88ed
Mips2648 <mips2648@gmail.com>
2025-03-14 13:30:54 +0100

    fix: adapt setuptool version

--- jeedomdaemon/__init__.py
+++ jeedomdaemon/__init__.py
@@ -1,6 +1,5 @@
 from .base_daemon import BaseDaemon
 from .base_config import BaseConfig
-from .version import VERSION
 
 __all__ = ["BaseDaemon", "BaseConfig"]
-__version__ = VERSION
+__version__ = "1.2.6"
